e264511e11c4207569fa352f431755c24e8d1718
Cammelier <car.iengo@stud.uniroma3.it>
2026-01-20 16:13:26 +0100

    fix issues

--- src/models.py
+++ src/models.py
@@ -34,21 +34,21 @@ class SimCLR(nn.Module):
         else: 
             self.classifier = nn.Identity()
 
-        def forward(self, x, return_features=False):
-            h = self.backbone(x)
+    def forward(self, x, return_features=False):
+        h = self.backbone(x)
 
-            if return_features:
-                return h
-            if not self.training and self.num_classes is not None:
-                return self.classifier(h)
+        if return_features:
+            return h
+        if not self.training and self.num_classes is not None:
+            return self.classifier(h)
 
-            # training with SupCon
-            if self.num_classes is not None:
-                z = self.projection(h)
-                z = F.normalize(z, dim=1)
-                class_logits = self.classifier(h)
-                return z, class_logits
-        
-            # Default: Training SimCLR 
+        # training with SupCon
+        if self.num_classes is not None:
             z = self.projection(h)
-            return F.normalize(z, dim=1)
+            z = F.normalize(z, dim=1)
+            class_logits = self.classifier(h)
+            return z, class_logits
+        
+        # Default: Training SimCLR 
+        z = self.projection(h)
+        return F.normalize(z, dim=1)
