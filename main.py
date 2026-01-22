6d337e922857fc97b593a417b415bd5abd5950e7
Cammelier <car.iengo@stud.uniroma3.it>
2026-01-22 17:15:33 +0100

    fix issues

--- main.py
+++ main.py
@@ -247,6 +247,10 @@ def main(cfg: DictConfig):
     # Build Model
     model = SimCLR(base_model=cfg.model.backbone, out_dim=cfg.model.out_dim).to(device)
 
+    if torch.cuda.device_count() > 1:
+        logger.info(f"🔥 ATTIVAZIONE MULTI-GPU: Trovate {torch.cuda.device_count()} GPU!")
+        model = nn.DataParallel(model)
+        
     # Execution Stages
     stage = cfg.get("stage", "all")
     
